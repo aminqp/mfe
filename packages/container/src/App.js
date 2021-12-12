d5d334b1a859c3fd77a6ee500ddb5a588058c7df
amin motamedi <aminqp@gmail.com>
2021-12-12 18:44:31 +0000

    create container deploy script

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import { StylesProvider } from '@material-ui/core/styles'
 
 
+
 import MarketingApp from "../component/MarketingApp";
 
 export default () => {
